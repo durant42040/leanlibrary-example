001ef8b9422f17697e70fbfa0dfbdef1aab6953a
durant42040 <b11901040@ntu.edu.tw>
2025-07-11 23:18:44 -0500

    add example

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-import leanlibrary
+from leanlibrary import LeanAgent
 
 def main():
-    print("Hello from test!")
+    url = "https://github.com/durant42040/lean4-example"
+    commit = "431b4a223095a5ea2d7d3761e6c50d14a87392c3"
+
+    agent = LeanAgent()
+
+    agent.process_repository(url, commit)
+
+    agent.train()
+    agent.evaluate()
+    agent.prove()
+
 
 
 if __name__ == "__main__":
